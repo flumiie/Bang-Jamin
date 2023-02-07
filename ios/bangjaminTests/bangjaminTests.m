768a387573be959053627f71bbef64384b340339
flumiie <ferickwar@gmail.com>
2023-02-07 21:20:21 +0700

    gitignore; Rename app

--- ios/bangjaminTests/bangjaminTests.m
+++ ios/bangjaminTests/bangjaminTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface bangjaminTests : XCTestCase
+@interface BangJaminTests : XCTestCase
 
 @end
 
-@implementation bangjaminTests
+@implementation BangJaminTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
